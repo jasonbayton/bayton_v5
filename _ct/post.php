478fcf01eb9ed5ed2874f6e700cf547b43714346
Jason Bayton <jason@bayton.org>
2021-02-18 14:17:43 +0000

    add the ct magic into the theme

--- _ct/post.php
+++ _ct/post.php
@@ -18,30 +18,19 @@
     <div id="featured_image">
     </div>
     <div id="literal_content" class="js-toc-content">
-      <p>As 2020 comes to a long-overdue close and we head into what anyone can hope will be a slightly less chaotic new year,
-      it also edges closer to what will soon be two years leading product with Social Mobile. With the results of all the hard work
-      now materialising, I think it’s interesting to reflect on the last >18 months, what it’s lead to, and what’s next.</p>
+      <?php
+      // Open post data
+      $content = file_get_contents("content/{$post["sourcefile"]}");
 
-      <p>Note: The following is a personal take on a work-related topic. For normal Android Enterprise content, please head here.</p>
+      // Parse post from markdown to HTML
+      require('libraries/parsedown.php');
+      $Parsedown = new Parsedown();
 
-      <h2>The leadup</h2>
-      <p>I’ve spent many years on the consumer end of Android, from devices purchased for my own use over the last decade, to those
-      I’ve reviewed in an enterprise context through my efforts on Android Enterprise. I have, and still continue to engage with OEMs
-      in the ecosystem for devices I test on a reasonably regular basis, pointing out issues with implementations, random bugs,
-      offering advice where asked and so forth, but that’s typically where it stops; once I’ve said my bit the feedback is taken internally
-      and tends to fall into the ether of internal triaging. If I’m lucky, a few weeks to a couple of months later I’ll get a ping to say a
-      software update addresses previous concerns, but more often than not that doesn’t happen.</p>
-      <h2>The leaddown</h2>
-      <p>With the lack of transparency also comes a few other common experiences – OEMs move slowly. Their engineering teams are
-      inaccessible or don’t speak to outside parties. Enterprise concepts and/or best practices are either not known or not prioritised,
-      and so on. Today in fairness it’s substantially better than it used to be through the education and motivation provided by
-      programmes such as Android Enterprise Recommended, but I could still earn a decent living consulting for OEMs if desired.</p>
-      <h3 id="1">R U listening</h3>
-      <p>In any case, after years of telling OEMs how things should be done with zero experience of the processes on the other side, I’d
-      often wondered what could really be involved in building Android devices, and if I could easily translate my knowledge of Android –
-      both Enterprise, including best practices, custom APIs, device management, etc, as well as exposure to several years of
-      communities such as XDA flashing ROMs and learning what tweaks to various rooted devices do what – to give me a solid rolling
-      start on the parts I knew less about – GMS, certifications, hardware requirements, testing, building from AOSP, etc.</p>
+      // Output post
+      ?><h2><?php echo $post['title'];?></h2>
+      <strong><i><?php echo $post['subtitle'];?></i></strong>
+      <?php echo $Parsedown->text($content);
+      ?>
     </div>
     <?php include "$themedir/_tc/share.php"; ?>
   </article>
