0d5129e77ba09296bb4a591efb6562a98734dd03
Jason Bayton <jason@bayton.org>
2021-02-19 17:04:12 +0000

    accessibility

--- _tc/nav.php
+++ _tc/nav.php
@@ -3,5 +3,5 @@
 </div>
 <div id="search">
   <span class="fa fa-search" id="search_icon"></span>
-  <input type="text" placeholder="What would you like to learn today?">
+  <input type="text" label="search" placeholder="What would you like to learn today?">
 </div>
