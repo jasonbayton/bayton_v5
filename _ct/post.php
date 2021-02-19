7f7f078cdeba43d2c6cc1a4d0bda77240026b1a5
Jason Bayton <jason@bayton.org>
2021-02-19 00:39:45 +0000

    copy pasta

--- _ct/post.php
+++ _ct/post.php
@@ -25,7 +25,24 @@
       $Parsedown = new Parsedown();
 
       // Output post
-      echo $Parsedown->text($content);
+      return $Parsedown->text($content);
+
+      /**
+      * Automatically add IDs to headings such as <h2></h2>
+      */
+      function auto_id_headings( $content ) {
+	       $content = preg_replace_callback( '/(\<h[1-6](.*?))\>(.*)(<\/h[1-6]>)/i', function( $matches ) {
+        	if ( ! stripos( $matches[0], 'id=' ) ) :
+          	$heading_link = '<a href="#' . sanitize_title( $matches[3] ) . '" class="heading-link"> <small><sup><i class="fa fa-bookmark text-grey"></i></sup></small></a> ';
+              $matches[0] = $matches[1] . $matches[2] . ' id="' . sanitize_title( $matches[3] ) . '">' . $heading_link . $matches[3]. $matches[4];
+          endif;
+          return $matches[0];
+        }, $content );
+    return $content;
+
+    echo $content;
+}
+
       ?>
     </div>
     <?php include "$themedir/_tc/share.php"; ?>
