43711c3d95af427b60c03345b6166db3e2a8a895
Jason Bayton <jason@bayton.org>
2021-02-21 22:27:49 +0000

    Nailing SVG shadow

--- _tc/featured_post.php
+++ _tc/featured_post.php
@@ -19,6 +19,7 @@
     </div>
     <svg class="angled-background-bottom" preserveAspectRatio="none" viewBox="0, 0, 100, 10">
     	<polygon class="left-triangle orange-cross" points="100,0 0,0, 100,10"></polygon>
+      <polygon class="shadow-triangle" points="0,10 100,0, 100,0.4"></polygon>
     	<polygon class="right-triangle" points="100,0 0,0, 0,10"></polygon>
     </svg>
   </section>
--- _tc/header.php
+++ _tc/header.php
@@ -22,5 +22,6 @@
 </div>
 <svg class="angled-background-bottom" preserveAspectRatio="none" viewBox="0, 0, 100, 10">
   <polygon class="left-triangle" points="100,0 0,0, 100,10"></polygon>
+  <polygon class="shadow-triangle" points="0,10 100,0, 100,0.4"></polygon>
   <polygon class="right-triangle" points="100,0 0,0, 0,10"></polygon>
 </svg>
