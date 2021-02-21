64a203bf7d92fb04bdbc1adfd4713cdcc46df9cc
Jason Bayton <jason@bayton.org>
2021-02-21 10:43:28 +0000

    typo

--- _tc/header.php
+++ _tc/header.php
@@ -1,4 +1,4 @@
-<div id="header_container primary-background" class="criss-cross">
+<div id="header_container" class="criss-cross primary-background">
     <div id="bayton">
       <div id="bayton_text">
         BAYTON
