d09bf08390e37305a8ba8fee98e7ee0c5dd00346
Jason Bayton <jason@bayton.org>
2021-02-21 10:16:54 +0000

    fixed divs and added new svg

--- _tc/header.php
+++ _tc/header.php
@@ -1,21 +1,24 @@
 <div id="header_container" class="criss-cross">
-  <div id="bayton">
-    <div id="bayton_text">
-      BAYTON
+  <div class="primary-background">
+    <div id="bayton">
+      <div id="bayton_text">
+        BAYTON
+      </div>
+    </div>
+    <nav>
+      <?php include "$themedir/_tc/nav.php"; ?>
+    </nav>
+    <div id="intro">
+      <img id="face" src="<?php echo $themerelpath ?>/img/jb.webp"/>
+      <h2 id="name">
+        Jason Bayton
+      </h2>
+      <h3 id="title">
+        Certified Android Enterprise Expert
+      </h3>
+      <span id="subtitle">
+        vExpert + Android Advocate + CPO
+      </span>
     </div>
   </div>
-  <nav>
-    <?php include "$themedir/_tc/nav.php"; ?>
-  </nav>
-  <div id="intro">
-    <img id="face" src="<?php echo $themerelpath ?>/img/jb.webp"/>
-    <h2 id="name">
-      Jason Bayton
-    </h2>
-    <h3 id="title">
-      Certified Android Enterprise Expert
-    </h3>
-    <span id="subtitle">
-      vExpert + Android Advocate + CPO
-    </span>
 </div>
