942d8e7b2ecddd5a1a680d894ab9dca83659b8f0
Jason Bayton <jason@bayton.org>
2021-02-21 10:16:37 +0000

    fixed divs and added new svg css

--- _tc/featured_post.php
+++ _tc/featured_post.php
@@ -1,12 +1,22 @@
 <section id="featured">
-  <div id="featured_container" class="criss-cross orange-cross criss-cross-mid">
-    <span id="featured_text">Featured article</span>
-    <h2 id="featured_article">
-      Android Enterprise vs Device Admin:
-      Why DA is no longer suitable
-    </h2>
-    <button id="embedded_readmore" class="button-blue button-rounded">
-      Read more <i class="fas fa-arrow-right"></i>
-    </button>
+  <svg class="angled-background-top" preserveAspectRatio="none" viewBox="0, 0, 100, 10">
+    <polygon class="left-triangle orange-cross" points="0,0 0,100 100,10"></polygon>
+    <polygon class="right-triangle" points="100,100 100,0 0,10"></polygon>
+  </svg>
+  <div class="primary-background">
+    <div id="featured_container" class="criss-cross orange-cross criss-cross-mid">
+      <span id="featured_text">Featured article</span>
+      <h2 id="featured_article">
+        Android Enterprise vs Device Admin:
+        Why DA is no longer suitable
+      </h2>
+      <button id="embedded_readmore" class="button-blue button-rounded">
+        Read more <i class="fas fa-arrow-right"></i>
+      </button>
+    </div>
   </div>
+  <svg class="angled-background-bottom" preserveAspectRatio="none" viewBox="0, 0, 100, 10">
+  	<polygon class="left-triangle orange-cross" points="100,0 0,0, 100,10"></polygon>
+  	<polygon class="right-triangle" points="100,0 0,0, 0,10"></polygon>
+  </svg>
 </section>
