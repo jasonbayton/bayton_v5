bbf05189d53d25d35a91e8168a620b6c1d3a3ac8
Jason Bayton <jason@bayton.org>
2021-02-21 10:20:08 +0000

    actually added the svgs

--- _tc/header.php
+++ _tc/header.php
@@ -21,4 +21,8 @@
       </span>
     </div>
   </div>
+  <svg class="angled-background-bottom" preserveAspectRatio="none" viewBox="0, 0, 100, 10">
+  	<polygon class="left-triangle" points="100,0 0,0, 100,10"></polygon>
+  	<polygon class="right-triangle" points="100,0 0,0, 0,10"></polygon>
+  </svg>
 </div>
