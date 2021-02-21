65d85943873a7079ae0ae7d0c7fbf95393904575
Jason Bayton <jason@bayton.org>
2021-02-21 12:13:06 +0000

    Add random number gen to CSS to get around mobile

--- _tc/head.php
+++ _tc/head.php
@@ -1,8 +1,9 @@
+<?php $randomnumber = mt_rand(100000,999999); ?>
 <meta charset = "utf-8">
 <title> Jason Bayton </title>
 <meta name = "description" content = "Jason Bayton">
 <meta name = "author" content = "Jason Bayton">
 <meta name = "viewport" content="width=device-width, initial-scale=1.0">
-<link rel = "stylesheet" href = "<?php echo $themerelpath ?>/css/core.css">
+<link rel = "stylesheet" href = "<?php echo $themerelpath ?>/css/core.css?v=<?php echo $randomnumber;?>">
 <link rel = "stylesheet" href = "<?php echo $themerelpath ?>/css/webfonts.css">
 <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
