33e0b74cc5011374745e35fbc08c0683c1b7df13
Jason Bayton <jason@bayton.org>
2021-02-22 16:34:15 +0000

    containering the aside modules so I can do stuff with them for mq

--- _tc/aside.php
+++ _tc/aside.php
@@ -1,20 +1,24 @@
 <div id="aside_container">
-  <div class="aside-title">
-    <h2>Meta</h2>
+  <div id="aside_meta">
+    <div class="aside-title">
+      <h2>Meta</h2>
+    </div>
+    <div class="aside-meta">
+      Published: <?php echo $post['shortmonth'];?> <?php echo $post['day'];?>, <?php echo $post['year'];?>
+    </div>
+    <div class="aside-meta">
+      Updated: <?php echo $post['updated'];?>
+    </div>
+    <div class="aside-meta">
+      Read time: 9 minutes
+    </div>
   </div>
-  <div class="aside-meta">
-    Published: <?php echo $post['shortmonth'];?> <?php echo $post['day'];?>, <?php echo $post['year'];?>
-  </div>
-  <div class="aside-meta">
-    Updated: <?php echo $post['updated'];?>
-  </div>
-  <div class="aside-meta">
-    Read time: 9 minutes
-  </div>
-  <div class="aside-title">
-    <h2>Contents</h2>
-  </div>
-  <div id="article_contents_list" class="js-toc">
-    <!-- Taken care of by Tocbot -->
+  <div id="aside_contents">
+    <div class="aside-title">
+      <h2>Contents</h2>
+    </div>
+    <div id="article_contents_list" class="js-toc">
+      <!-- Taken care of by Tocbot -->
+    </div>
   </div>
 </div>
