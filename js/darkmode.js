e4109e5d8a0431e932ecaf85d28216a3a2f78809
Jason Bayton <jason@bayton.org>
2021-02-25 16:36:35 +0000

    revert

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,3 +1,3 @@
 document.getElementById('darktoggle').addEventListener('click', function(event){
-  (event.target.clicked) ? document.body.setAttribute('data-theme', 'dark') : document.body.removeAttribute('data-theme');
+  (event.target) ? document.body.setAttribute('data-theme', 'dark') : document.body.removeAttribute('data-theme');
 });
