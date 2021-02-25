94c28447a024b1a1f091a8a7f7ae373acdce13c8
Jason Bayton <jason@bayton.org>
2021-02-25 21:22:00 +0000

    darkmode js fixes

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,3 +1,16 @@
-document.getElementById('darktoggle').addEventListener('click', function(event){
-  (event.target) ? document.body.setAttribute('data-theme', 'dark') : document.body.removeAttribute('data-theme');
-});
+var toggle = document.getElementById("darktoggle");
+  var storedTheme = localStorage.getItem('theme') || (window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light");
+  if (storedTheme)
+    document.documentElement.setAttribute('data-theme', storedTheme)
+
+  toggle.onclick = function() {
+    var currentTheme = document.documentElement.getAttribute("data-theme");
+    var targetTheme = "light";
+
+    if (currentTheme === "light") {
+      targetTheme = "dark";
+    }
+
+  document.documentElement.setAttribute('data-theme', targetTheme)
+  localStorage.setItem('theme', targetTheme);
+};
