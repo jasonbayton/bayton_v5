1705bc082c7e6529377ea39ccc355140ffcf7d90
Jason Bayton <jason@bayton.org>
2021-03-01 11:48:25 +0000

    convert to template

--- _tc/latest_posts.php
+++ _tc/latest_posts.php
@@ -1,117 +1,37 @@
 <section id="latest_posts">
-
-<?php
-usort($posts, fn($a, $b) => strcmp($b['date'], $a['date']));
-for($i = 0; $i < 5; $i++){
-  echo $posts[$i]['title'];
-} ?>
-
   <h2 id="latest_title">
     Read the latest
   </h2>
-  <div id="latest_posts_container">
-    <div class="a-post">
-      <div class="latest-posts-date">
-        <div class="the-date">
-          13th Dec 2020
-        </div>
-      </div>
-      <div class="latest-posts-categories">
-        <span class="the-category">
-          Enterprise
-        </span>
-        <span class="the-category">
-          Android
-        </span>
-      </div>
-      <div class="latest-posts-title">
-        <h3 class="latest-title">
-          Listen again: BM podcast #144 – Jason Bayton & Russ Mohr talk Android!
-        </h3>
-      </div>
-      <div class="latest-posts-subtitle">
-        <span class="the-subtitle">
-          This is a subtitle
-        </span>
-      </div>
-    </div>
 
-    <div class="a-post">
-      <div class="latest-posts-date">
-        <div class="the-date">
-          13th Dec 2020
+  <?php
+    usort($posts, fn($a, $b) => strcmp($b['date'], $a['date']));
+    for($i = 0; $i < 5; $i++){ ?>
+      echo $posts[$i]['title'];
+      <div id="latest_posts_container">
+        <div class="a-post">
+          <div class="latest-posts-date">
+            <div class="the-date">
+              <?php echo $posts[$i]['date']; ?>
+            </div>
+          </div>
+          <div class="latest-posts-categories">
+            <span class="the-category">
+              <?php echo $posts[$i]['categories']; ?>
+            </span>
+          </div>
+          <div class="latest-posts-title">
+            <h3 class="latest-title">
+              <?php echo $posts[$i]['title']; ?>
+            </h3>
+          </div>
+          <div class="latest-posts-subtitle">
+            <span class="the-subtitle">
+              <?php echo $posts[$i]['subtitle']; ?>
+            </span>
+          </div>
         </div>
-      </div>
-      <div class="latest-posts-categories">
-        <span class="the-category">
-          Enterprise
-        </span>
-        <span class="the-category">
-          Android
-        </span>
-      </div>
-      <div class="latest-posts-title">
-        <h3 class="latest-title">
-          On building Android devices
-        </h3>
-      </div>
-      <div class="latest-posts-subtitle">
-        <span class="the-subtitle">
-          This is a subtitle
-        </span>
-      </div>
-    </div>
-    <div class="a-post">
-      <div class="latest-posts-date">
-        <div class="the-date">
-          13th Dec 2020
-        </div>
-      </div>
-      <div class="latest-posts-categories">
-        <span class="the-category">
-          Enterprise
-        </span>
-        <span class="the-category">
-          Android
-        </span>
-      </div>
-      <div class="latest-posts-title">
-        <h3 class="latest-title">
-          Google announce big changes to zero-touch
-        </h3>
-      </div>
-      <div class="latest-posts-subtitle">
-        <span class="the-subtitle">
-          This is a subtitle
-        </span>
-      </div>
-    </div>
-    <div class="a-post">
-      <div class="latest-posts-date">
-        <div class="the-date">
-          13th Dec 2020
-        </div>
-      </div>
-      <div class="latest-posts-categories">
-        <span class="the-category">
-          Enterprise
-        </span>
-        <span class="the-category">
-          Android
-        </span>
-      </div>
-      <div class="latest-posts-title">
-        <h3 class="latest-title">
-          Workspace ONE UEM 1810 introduces support for Android Enterprise
-          fully managed devices with work profiles
-      </div>
-      <div class="latest-posts-subtitle">
-        <span class="the-subtitle">
-          This is a subtitle
-        </span>
-      </div>
-    </div>
-  </div>
+    <?php } ?>
+
   <button id="read_more" class="button-blue button-rounded">
     Read more <i class="fas fa-arrow-right"></i>
   </button>
