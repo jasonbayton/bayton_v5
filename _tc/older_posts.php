0a199f0e96216470458d01ddfd31785c028897bc
Jason Bayton <jason@bayton.org>
2021-03-01 18:57:43 +0000

    fixed the query

--- _tc/older_posts.php
+++ _tc/older_posts.php
@@ -13,7 +13,6 @@
           <ul class="older-posts-list">
             <?php
             foreach ($posts as $query){
-              echo $query;
               if (in_array('enterprise', $query['categories'])) {
                 echo '<li>' . $query['title'] . '</li>';
               }
