2cea4fd6fe3df4b2c6335ee86aa364eff36bb3f3
Jason Bayton <jason@bayton.org>
2021-03-02 14:37:21 +0000

    tidy up

--- _tc/older_posts.php
+++ _tc/older_posts.php
@@ -12,20 +12,12 @@
           </h3>
           <ul class="older-posts-list">
             <?php
-            $list_string = file_get_contents('content/content.json');
-            $lists = json_decode($list_string, true, 255, JSON_THROW_ON_ERROR | JSON_INVALID_UTF8_IGNORE);
-            echo "<pre>";
-            print_r($lists);
-            echo "</pre>";
             $i=0;
-            foreach ($lists as $query){
+            foreach ($posts as $query){
               if (in_array('enterprise', $query['categories'])) {
                 $i++;
-                if($i==4) break;
+                if($i==6) break;
                 echo '<li>' . $query['title'] . '</li>';
-                echo "<pre>";
-                print_r($query);
-                echo "</pre>";
               };
             } ?>
           </ul>
@@ -40,12 +32,12 @@
           <ul class="older-posts-list">
             <?php
             $i=0;
-            foreach ($lists as $query){
+            foreach ($posts as $query){
               if (in_array('enterprise', $query['categories'])) {
+                $i++;
+                if($i==6) break;
                 echo '<li>' . $query['title'] . '</li>';
               };
-            $i++;
-            if($i==3) break;
             } ?>
           </ul>
           <button class="arrow-only button-white button-rounded">
@@ -59,12 +51,12 @@
           <ul class="older-posts-list">
             <?php
             $i=0;
-            foreach ($lists as $query){
+            foreach ($posts as $query){
               if (in_array('enterprise', $query['categories'])) {
+                $i++;
+                if($i==6) break;
                 echo '<li>' . $query['title'] . '</li>';
               };
-            $i++;
-            if($i==3) break;
             } ?>
           </ul>
           <button class="arrow-only button-white button-rounded">
