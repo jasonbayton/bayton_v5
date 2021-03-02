4098ee214c1f20d36c2d8f648976872d821def7c
Jason Bayton <jason@bayton.org>
2021-03-02 18:55:03 +0000

    mis-paste

--- _tc/latest_posts.php
+++ _tc/latest_posts.php
@@ -8,11 +8,11 @@
     usort($posts, fn($a, $b) => strcmp($b['date'], $a['date']));
     $i=0;
     foreach ($posts as $url => $query){
-      print_r($url);
-      ?>
+            print_r($url);
       $transDate = date("M d, Y", strtotime($query['date']));
       $i++;
       if($i==6) break;
+      ?>
         <div class="a-post">
           <div class="latest-posts-date">
             <div class="the-date">
