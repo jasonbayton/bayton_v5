6711234a93ce06cfea191508b569d5e22c63fdf8
Jason Bayton <jason@bayton.org>
2021-03-02 16:53:26 +0000

    redo the latest post query for URL support

--- _tc/latest_posts.php
+++ _tc/latest_posts.php
@@ -3,14 +3,14 @@
     Read the latest
   </h2>
 
+      <div id="latest_posts_container">
   <?php
     usort($posts, fn($a, $b) => strcmp($b['date'], $a['date']));
-    for($i = 0; $i < 5; $i++){
-
-      $transDate = date("M d, Y", strtotime($posts[$i]['date']));
+    $i=0;
+    foreach ($posts as $url => $query){
+      $transDate = date("M d, Y", strtotime($query['date']));
       ?>
 
-      <div id="latest_posts_container">
         <div class="a-post">
           <div class="latest-posts-date">
             <div class="the-date">
@@ -18,22 +18,22 @@
             </div>
           </div>
           <div class="latest-posts-categories">
-          <?php foreach ($posts[$i]['categories'] as $category) {
+          <?php foreach ($query['categories'] as $category) {
               echo '<span class="the-category">' . $category .'</span>'; }; ?>
           </div>
           <div class="latest-posts-title">
             <h3 class="latest-title">
-              <?php echo $posts[$i]['title']; ?>
+              <?php echo '<a href="' .$url. '">' .$query['title'];. '</a>' ?>
             </h3>
           </div>
           <div class="latest-posts-subtitle">
             <span class="the-subtitle">
-              <?php echo $posts[$i]['subtitle']; ?>
+              <?php echo $query['subtitle']; ?>
             </span>
           </div>
-        </div>
-    <?php } ?>
 
+    <?php } ?>
+        </div>
   <button id="read_more" class="button-blue button-rounded">
     Read more <i class="fas fa-arrow-right"></i>
   </button>
