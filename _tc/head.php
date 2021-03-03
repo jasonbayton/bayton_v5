623e0aa34b647e6b0f0dde9a7be68fa9c031ccd6
Jason Bayton <jason@bayton.org>
2021-03-03 14:08:10 +0000

    meta tags

--- _tc/head.php
+++ _tc/head.php
@@ -2,7 +2,8 @@
 <meta charset = "utf-8">
 <title> Jason Bayton </title>
 <meta name = "description" content = "Jason Bayton">
-<meta name = "author" content = "Jason Bayton">
+<meta name = "author" content = "Jason Bayton | <?php echo $post['title'];?> ">
+<meta name="twitter:site" content="@jasonbayton" />
 <meta name = "viewport" content="width=device-width, initial-scale=1.0">
 <script src="<?php echo $themerelpath ?>/js/darkmode.js"></script>
 <link rel = "stylesheet" href = "<?php echo $themerelpath ?>/css/core.css?v=<?php echo $randomnumber;?>">
