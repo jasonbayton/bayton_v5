03cb69c056f877aa992ef59cbc2b13804e1d8c30
Jason Bayton <jason@bayton.org>
2021-03-03 14:42:04 +0000

    meta & heropic (OG)

--- _tc/head.php
+++ _tc/head.php
@@ -1,10 +1,15 @@
 <?php $randomnumber = mt_rand(100000,999999); ?>
 <meta charset = "utf-8">
-<title> Jason Bayton </title>
-<meta name = "description" content = "Jason Bayton">
-<meta name = "author" content = "Jason Bayton | <?php echo $post['title'];?> ">
+<title> Jason Bayton | <?php echo $post['title'];?></title>
+<meta name = "description" content = "<?php echo $post['subtitle'];?>">
+<meta name = "author" content = "Jason Bayton ">
 <meta name="twitter:site" content="@jasonbayton" />
 <meta name = "viewport" content="width=device-width, initial-scale=1.0">
+<meta property="og:title" content="Jason Bayton | <?php echo $post['title'];?>"/>
+<meta property="og:description" content="<?php echo $post['subtitle'];?>"/>
+<meta property="og:type" content="website"/>
+<meta property="og:url" content="https://<?php echo $domain;?>"/>
+<meta property="og:image" content="<?php echo $themerelpath?>/img/bayton.webp"/>
 <script src="<?php echo $themerelpath ?>/js/darkmode.js"></script>
 <link rel = "stylesheet" href = "<?php echo $themerelpath ?>/css/core.css?v=<?php echo $randomnumber;?>">
 <link rel = "stylesheet" href = "<?php echo $themerelpath ?>/css/webfonts.css">
