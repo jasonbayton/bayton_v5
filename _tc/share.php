8f9ca9f426df697edec7c781570730f1e092774b
Jason Bayton <jason@bayton.org>
2021-03-03 11:35:00 +0000

    update share icon colours

--- _tc/share.php
+++ _tc/share.php
@@ -1,7 +1,11 @@
 <div id="share_container">
   <hr id="share_hr">
   <div id="share_icons">
-    <span style="color:#3174E8;"><i class="fab fa-linkedin"></i></span> <span style="color:#3BF0F0;"><i class="fab fa-twitter-square"></i></span> <span style="color:#15C72D;"><i class="fab fa-whatsapp-square"></i></span> <span style="color:#1C2DA2;"><i class="fab fa-facebook-square"></i></span> <span style="color:red;"><i class="fas fa-envelope-square"></i></span>
+    <span id="share_li"><i class="fab fa-linkedin"></i></span>
+    <span id="share_tw"><i class="fab fa-twitter-square"></i></span>
+    <span id="share_wa"><i class="fab fa-whatsapp-square"></i></span>
+    <span id="share_fb"><i class="fab fa-facebook-square"></i></span>
+    <span id="share_em"><i class="fas fa-envelope-square"></i></span>
   </div>
 </div>
 <div id="content_report">
