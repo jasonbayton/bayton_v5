0d5964e3a5ba223b398f0620063a49eb89f9009f
Jason Bayton <jason@bayton.org>
2021-03-03 14:02:14 +0000

    comments

--- _tc/breadcrumb.php
+++ _tc/breadcrumb.php
@@ -1,3 +1,9 @@
 <div id="breadcrumb">
   <?php echo $post['topic'];?> &#9654; <?php echo $post['parent'];?>
 </div>
+
+<!--
+To come later - if these don't exist, don't show anything.
+If parent isn't under a topic directly, add the 3 dots ...
+And add categories, linked, here also.
+-->
