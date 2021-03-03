51ac45354d8f18f5f9ef6be8a0de930ebfcfaffb
Jason Bayton <jason@bayton.org>
2021-03-03 18:10:14 +0000

    update to theme

--- _ct/archive.php
+++ _ct/archive.php
@@ -3,215 +3,34 @@
     Archive
   </h2>
   <div id="archive_posts_container">
-    <div class="a-post">
-      <div class="latest-posts-date">
-        <div class="the-date">
-          13th Dec 2020
-        </div>
-      </div>
-      <div class="latest-posts-categories">
-        <span class="the-category">
-          Enterprise
-        </span>
-        <span class="the-category">
-          Android
-        </span>
-      </div>
-      <div class="latest-posts-title">
-        <h3 class="latest-title">
-          Listen again: BM podcast #144 – Jason Bayton & Russ Mohr talk Android!
-        </h3>
-      </div>
-      <div class="latest-posts-subtitle">
-        <span class="the-subtitle">
-          This is a subtitle
-        </span>
-      </div>
-    </div>
 
-    <div class="a-post">
-      <div class="latest-posts-date">
-        <div class="the-date">
-          13th Dec 2020
-        </div>
-      </div>
-      <div class="latest-posts-categories">
-        <span class="the-category">
-          Enterprise
-        </span>
-        <span class="the-category">
-          Android
-        </span>
-      </div>
-      <div class="latest-posts-title">
-        <h3 class="latest-title">
-          On building Android devices
-        </h3>
-      </div>
-      <div class="latest-posts-subtitle">
-        <span class="the-subtitle">
-          This is a subtitle
-        </span>
-      </div>
-    </div>
-    <div class="a-post">
-      <div class="latest-posts-date">
-        <div class="the-date">
-          13th Dec 2020
-        </div>
-      </div>
-      <div class="latest-posts-categories">
-        <span class="the-category">
-          Enterprise
-        </span>
-        <span class="the-category">
-          Android
-        </span>
-      </div>
-      <div class="latest-posts-title">
-        <h3 class="latest-title">
-          Google announce big changes to zero-touch
-        </h3>
-      </div>
-      <div class="latest-posts-subtitle">
-        <span class="the-subtitle">
-          This is a subtitle
-        </span>
-      </div>
-    </div>
-    <div class="a-post">
-      <div class="latest-posts-date">
-        <div class="the-date">
-          13th Dec 2020
-        </div>
-      </div>
-      <div class="latest-posts-categories">
-        <span class="the-category">
-          Enterprise
-        </span>
-        <span class="the-category">
-          Android
-        </span>
-      </div>
-      <div class="latest-posts-title">
-        <h3 class="latest-title">
-          Workspace ONE UEM 1810 introduces support for Android Enterprise
-          fully managed devices with work profiles
-      </div>
-      <div class="latest-posts-subtitle">
-        <span class="the-subtitle">
-          This is a subtitle
-        </span>
-      </div>
-    </div>
-    <div class="a-post">
-      <div class="latest-posts-date">
-        <div class="the-date">
-          13th Dec 2020
-        </div>
-      </div>
-      <div class="latest-posts-categories">
-        <span class="the-category">
-          Enterprise
-        </span>
-        <span class="the-category">
-          Android
-        </span>
-      </div>
-      <div class="latest-posts-title">
-        <h3 class="latest-title">
-          Listen again: BM podcast #144 – Jason Bayton & Russ Mohr talk Android!
-        </h3>
-      </div>
-      <div class="latest-posts-subtitle">
-        <span class="the-subtitle">
-          This is a subtitle
-        </span>
-      </div>
-    </div>
+    <?php
+      usort($posts, fn($a, $b) => strcmp($b['date'], $a['date']));
+      foreach ($posts as $query){
+        $transDate = date("M d, Y", strtotime($query['date']));
+        ?>
+          <div class="a-post">
+            <div class="latest-posts-date">
+              <div class="the-date">
+                <?php echo $transDate; ?>
+              </div>
+            </div>
+            <div class="latest-posts-categories">
+            <?php foreach ($query['categories'] as $category) {
+                echo '<span class="the-category">' . $category .'</span>'; }; ?>
+            </div>
+            <div class="latest-posts-title">
+              <h3 class="latest-title">
+                <?php echo '<a href="' . $query['url'] . '">' . $query['title'] . '</a>' ?>
+              </h3>
+            </div>
+            <div class="latest-posts-subtitle">
+              <span class="the-subtitle">
+                <?php echo $query['subtitle']; ?>
+              </span>
+            </div>
+          </div>
+      <?php } ?>
 
-    <div class="a-post">
-      <div class="latest-posts-date">
-        <div class="the-date">
-          13th Dec 2020
-        </div>
-      </div>
-      <div class="latest-posts-categories">
-        <span class="the-category">
-          Enterprise
-        </span>
-        <span class="the-category">
-          Android
-        </span>
-      </div>
-      <div class="latest-posts-title">
-        <h3 class="latest-title">
-          On building Android devices
-        </h3>
-      </div>
-      <div class="latest-posts-subtitle">
-        <span class="the-subtitle">
-          This is a subtitle
-        </span>
-      </div>
-    </div>
-    <div class="a-post">
-      <div class="latest-posts-date">
-        <div class="the-date">
-          13th Dec 2020
-        </div>
-      </div>
-      <div class="latest-posts-categories">
-        <span class="the-category">
-          Enterprise
-        </span>
-        <span class="the-category">
-          Android
-        </span>
-      </div>
-      <div class="latest-posts-title">
-        <h3 class="latest-title">
-          Google announce big changes to zero-touch
-        </h3>
-      </div>
-      <div class="latest-posts-subtitle">
-        <span class="the-subtitle">
-          This is a subtitle
-        </span>
-      </div>
-    </div>
-    <div class="a-post">
-      <div class="latest-posts-date">
-        <div class="the-date">
-          13th Dec 2020
-        </div>
-      </div>
-      <div class="latest-posts-categories">
-        <span class="the-category">
-          Enterprise
-        </span>
-        <span class="the-category">
-          Android
-        </span>
-      </div>
-      <div class="latest-posts-title">
-        <h3 class="latest-title">
-          Workspace ONE UEM 1810 introduces support for Android Enterprise
-          fully managed devices with work profiles
-      </div>
-      <div class="latest-posts-subtitle">
-        <span class="the-subtitle">
-          This is a subtitle
-        </span>
-      </div>
-    </div>
-  </div>
-    <div class="button-group">
-      <button id="back_button" class="button-blue button-rounded">
-        <i class="fas fa-arrow-left"></i> Back
-      </button>
-      <button id="next_button" class="button-blue button-rounded">
-        Next <i class="fas fa-arrow-right"></i>
-      </button>
     </div>
   </section>
