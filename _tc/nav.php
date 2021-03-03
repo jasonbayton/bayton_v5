6f3af01ab997a86bff3aef57f3b24c375720d05b
Jason Bayton <jason@bayton.org>
2021-03-03 13:42:36 +0000

    replace fontawesome with utf

--- _tc/nav.php
+++ _tc/nav.php
@@ -2,8 +2,8 @@
   <a href="/about">About</a> <a href="/contact">Contact</a>
 </div>
 <div id="darktoggle">
-  <i class="fas fa-adjust"></i>
-</button>
+  &#9680;
+</div>
 <div id="search">
   <span class="fa fa-search" id="search_icon"></span>
   <input type="text" label="search" placeholder="What would you like to learn today?">
