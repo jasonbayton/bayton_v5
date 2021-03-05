890fc22e3e6fb291fd75c28a59cf22ced5711ffc
Jason Bayton <jason@bayton.org>
2021-03-05 21:47:06 +0000

    remove unused contents

--- index.php
+++ index.php
@@ -8,9 +8,7 @@
     <?php include "_tc/header.php"; ?>
   </header>
   <content>
-    <!-- php if page is home, else -->
     <?php include "_tc/content.php"; ?>
-    <!--?php include "_tc/archive_list.php"; ?-->
   </content>
   <footer>
     <?php require "_tc/footer.php"; ?>
