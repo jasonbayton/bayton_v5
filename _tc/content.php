9b09a246a0e1648b6446917bee551eaa145ab9f0
Jason Bayton <jason@bayton.org>
2021-03-07 22:30:24 +0000

    remove 404 from content into site

--- _tc/content.php
+++ _tc/content.php
@@ -3,11 +3,6 @@
 <div class="max-width">
 <?php
 
-if (!array_key_exists($url, $posts)) {
-    header('HTTP/1.1 404 Not Found');
-    die('Page not found: ' . $_GET['args']);
-}
-
 $published = date("M d, Y", strtotime($post['date']));
 $updated = date("M d, Y", strtotime($post['updated']));
 
