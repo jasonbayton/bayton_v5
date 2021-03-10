d5ceedc0d013c433125185612ed7e440885318ec
Jason Bayton <jason@bayton.org>
2021-03-10 15:47:32 +0000

    debugging

--- _tc/content.php
+++ _tc/content.php
@@ -7,6 +7,7 @@ $published = date("M d, Y", strtotime($post['date']));
 $updated = date("M d, Y", strtotime($post['updated']));
 
 include ("$themedir/_ct/{$post['type']}.php");
-echo $post['type'];
+echo '<!-- Content type:' . $post['type'] . '-->';
+echo '<!-- Content type:' . $post[$url] . '-->';
 ?>
 </div>
