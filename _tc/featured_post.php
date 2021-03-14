08e11373477ac37b5e755a04cdb3aa872d3a9041
Jason Bayton <jason@bayton.org>
2021-03-14 22:08:30 +0000

    fixed up layout breaking the link

--- _tc/featured_post.php
+++ _tc/featured_post.php
@@ -9,22 +9,21 @@
     <div class="primary-background">
       <div id="featured_container" class="criss-cross orange-cross criss-cross-mid">
         <span id="featured_text">Featured article</span>
-        <h2 id="featured_article">
-          <?php
-          $i=0;
-          foreach ($posts as $query){
-            if ($query['featured'] == 'true' && $query['published'] == "true"){
-              $i++;
-              if($i==2) break;
-              echo $query['title'];
-            }
-          } ?>
-        </h2>
-        <button id="embedded_readmore" class="button-blue button-rounded">
-          <a href="<?php echo $query['url'] ?>">Read more</a></i>
-        </button>
+        <?php
+        $i=0;
+        foreach ($posts as $query){
+          if ($query['featured'] == 'true' && $query['published'] == "true"){
+            $i++;
+            if($i==2) break; ?>
+            <h2 id="featured_article">
+              <?php echo $query['title']; ?>
+            </h2>
+            <button id="embedded_readmore" class="button-blue button-rounded">
+              <a href="<?php echo $query['url'] ?>">Read more</a></i>
+            </button>
+            <?php }} ?>
+          </div>
       </div>
-    </div>
     <svg class="angled-background-bottom" preserveAspectRatio="none" viewBox="0, 0, 100, 10">
     	<polygon class="left-triangle orange-cross" points="100,0 0,0, 100,10"></polygon>
       <polygon class="shadow-triangle" points="0,10 100,0, 100,0.4"></polygon>
