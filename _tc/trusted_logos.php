4823bd28e5589cf1bb427c68b1d7eb46ed912e69
Jason Bayton <jason@bayton.org>
2021-03-29 23:39:01 +0100

    add in trusted logos

---
+++ _tc/trusted_logos.php
@@ -0,0 +1,12 @@
+</div> <!-- Killing .max-width-->
+<section id="trusted_logos">
+  <h2 id="trusted_title">
+    Trusted content for 200,000 vendors, partners, and organisations a year
+  </h2>
+  <div id="trusted_container">
+  <div class="vendor-logo">
+    <img />
+  </div>
+  </div>
+</section>
+<div class="max-width"> <!-- Reviving max-width -->
