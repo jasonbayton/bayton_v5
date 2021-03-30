0983ff600eb7bdb10ff18d6140f0f74dd74c3f7c
Jason Bayton <jason@bayton.org>
2021-03-31 00:33:46 +0100

    logo design

--- _tc/trusted_logos.php
+++ _tc/trusted_logos.php
@@ -4,29 +4,31 @@
     <h2 id="trusted_title">
       Trusted content for 200,000 vendors, partners, and organisations a year
     </h2>
-    <div class="vendor-logo">
-      <img alt="citrix logo" src="<?php echo $themerelpath ?>/img/cit.png"/>
-    </div>
-    <div class="vendor-logo">
-      <img alt="mobco logo" src="<?php echo $themerelpath ?>/img/mob.png"/>
-    </div>
-    <div class="vendor-logo">
-      <img alt="microsoft logo" src="<?php echo $themerelpath ?>/img/ms.png"/>
-    </div>
-    <div class="vendor-logo">
-      <img alt="vodafone logo" src="<?php echo $themerelpath ?>/img/vod.webp"/>
-    </div>
-    <div class="vendor-logo">
-      <img alt="vmware logo" src="<?php echo $themerelpath ?>/img/vm.png"/>
-    </div>
-    <div class="vendor-logo">
-      <img alt="hmd logo" src="<?php echo $themerelpath ?>/img/hmd.png"/>
-    </div>
-    <div class="vendor-logo">
-      <img alt="hmd logo" src="<?php echo $themerelpath ?>/img/mi.png"/>
-    </div>
-    <div class="vendor-logo">
-      <img alt="hmd logo" src="<?php echo $themerelpath ?>/img/sam.png"/>
+    <div id="vendor_logo_container">
+      <div class="vendor-logo">
+        <img alt="citrix logo" src="<?php echo $themerelpath ?>/img/cit.png"/>
+      </div>
+      <div class="vendor-logo">
+        <img alt="mobco logo" src="<?php echo $themerelpath ?>/img/mob.png"/>
+      </div>
+      <div class="vendor-logo">
+        <img alt="microsoft logo" src="<?php echo $themerelpath ?>/img/ms.png"/>
+      </div>
+      <div class="vendor-logo">
+        <img alt="vodafone logo" src="<?php echo $themerelpath ?>/img/vod.webp"/>
+      </div>
+      <div class="vendor-logo">
+        <img alt="vmware logo" src="<?php echo $themerelpath ?>/img/vm.png"/>
+      </div>
+      <div class="vendor-logo">
+        <img alt="hmd logo" src="<?php echo $themerelpath ?>/img/hmd.png"/>
+      </div>
+      <div class="vendor-logo">
+        <img alt="hmd logo" src="<?php echo $themerelpath ?>/img/mi.png"/>
+      </div>
+      <div class="vendor-logo">
+        <img alt="hmd logo" src="<?php echo $themerelpath ?>/img/sam.png"/>
+      </div>
     </div>
   </div>
 </section>
