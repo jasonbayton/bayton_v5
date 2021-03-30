806bd280f4bedff3a03e1435aefa1142a6eda433
Jason Bayton <jason@bayton.org>
2021-03-31 00:07:31 +0100

    move title in container

--- _tc/trusted_logos.php
+++ _tc/trusted_logos.php
@@ -1,9 +1,9 @@
 </div> <!-- Killing .max-width-->
 <section id="trusted_logos">
+  <div id="trusted_container">
   <h2 id="trusted_title">
     Trusted content for 200,000 vendors, partners, and organisations a year
   </h2>
-  <div id="trusted_container">
   <div class="vendor-logo">
     <img />
   </div>
