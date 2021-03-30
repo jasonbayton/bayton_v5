aa0520cf1d460ccde35449db003489cefebc0893
Jason Bayton <jason@bayton.org>
2021-03-31 00:18:18 +0100

    logos & tl

--- _tc/trusted_logos.php
+++ _tc/trusted_logos.php
@@ -1,12 +1,27 @@
 </div> <!-- Killing .max-width-->
 <section id="trusted_logos">
   <div id="trusted_container">
-  <h2 id="trusted_title">
-    Trusted content for 200,000 vendors, partners, and organisations a year
-  </h2>
-  <div class="vendor-logo">
-    <img />
-  </div>
+    <h2 id="trusted_title">
+      Trusted content for 200,000 vendors, partners, and organisations a year
+    </h2>
+    <div class="vendor-logo">
+      <img alt="citrix logo" src="img/cit.png"/>
+    </div>
+    <div class="vendor-logo">
+      <img alt="mobco logo" src="img/mob.png"/>
+    </div>
+    <div class="vendor-logo">
+      <img alt="microsoft logo" src="img/ms.png"/>
+    </div>
+    <div class="vendor-logo">
+      <img alt="vodafone logo" src="img/vod.webp"/>
+    </div>
+    <div class="vendor-logo">
+      <img alt="vmware logo" src="img/vm.png"/>
+    </div>
+    <div class="vendor-logo">
+      <img alt="hmd logo" src="img/hmd.png"/>
+    </div>
   </div>
 </section>
 <div class="max-width"> <!-- Reviving max-width -->
