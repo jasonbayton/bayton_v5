300868fe2e13ba87e23f150026f1c377d88e6933
Jason Bayton <jason@bayton.org>
2021-03-31 00:24:51 +0100

    logos

--- _tc/trusted_logos.php
+++ _tc/trusted_logos.php
@@ -22,6 +22,9 @@
     <div class="vendor-logo">
       <img alt="hmd logo" src="<?php echo $themerelpath ?>/img/hmd.png"/>
     </div>
+    <div class="vendor-logo">
+      <img alt="hmd logo" src="<?php echo $themerelpath ?>/img/mi.png"/>
+    </div>
   </div>
 </section>
 <div class="max-width"> <!-- Reviving max-width -->
