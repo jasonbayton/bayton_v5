b45240c969e2e225426f6da9131e671bafdc3586
Jason Bayton <jason@bayton.org>
2021-03-31 22:54:40 +0100

    april fools

--- _tc/fools/header.php
+++ _tc/fools/header.php
@@ -13,7 +13,7 @@
       <?php include "$themedir/_tc/nav.php"; ?>
     </nav>
     <div id="intro">
-      <img id="face" alt="profile pic" src="<?php echo $themerelpath ?>/img/jb.webp"/>
+      <img id="face" alt="profile pic" src="<?php echo $themerelpath ?>/img/aplme.png"/>
       <h2 id="name">
         Jason Bayton
       </h2>
