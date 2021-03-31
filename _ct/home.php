389b939a6d36b0cb6342f45301732be18dfb0daf
Jason Bayton <jason@bayton.org>
2021-03-31 01:15:32 +0100

    positioning

--- _ct/home.php
+++ _ct/home.php
@@ -1,4 +1,5 @@
 <?php include "$themedir/_tc/grandiose_statement.php"; ?>
+<?php include "$themedir/_tc/trusted_logos.php"; ?>
 <?php include "$themedir/_tc/featured_post.php"; ?>
 <?php include "$themedir/_tc/latest_posts.php"; ?>
 <?php include "$themedir/_tc/trusted_logos.php"; ?>
