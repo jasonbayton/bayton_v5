97bb55ad50c5511b04a37d23b04fa61ca5f5cd99
Jason Bayton <jason@bayton.org>
2021-03-31 16:03:18 +0100

    textgroup

--- _tc/trusted_logos.php
+++ _tc/trusted_logos.php
@@ -5,11 +5,13 @@
   </svg-->
   <div class="purple-background">
     <div id="trusted_container">
-      <h2 id="trusted_title">
-        Relevant, up to date Android content for 200,000 visitors annually
-      </h2>
-      <div id="trusted_tag">
-        Check out some of the vendors and organisations I've assisted or collaborated with
+      <div id="trusted_textgroup">
+        <h2 id="trusted_title">
+          Relevant, up to date Android content for 200,000 visitors annually
+        </h2>
+        <div id="trusted_tag">
+          Check out some of the vendors and organisations I've assisted or collaborated with
+        </div>
       </div>
       <div id="vendor_logo_container">
         <div class="vendor-logo">
