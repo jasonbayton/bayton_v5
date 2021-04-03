57b9ceb553c81ec00f3024517fbd43fb34c20c95
Jason Bayton <jason@bayton.org>
2021-04-03 23:24:52 +0100

    bit more testing

--- _tc/trusted_logos.php
+++ _tc/trusted_logos.php
@@ -15,7 +15,7 @@
       </div>
       <div id="vendor_logo_container">
         <?php
-        $all_images = glob("{$themerelpath}/img/logos/{*.*}", GLOB_BRACE);
+        $all_images = glob("{$themerelpath}/img/logos/*.*", GLOB_BRACE);
         shuffle($all_images);
         $images = array();
         $i=0;
