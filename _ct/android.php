3e55417f57c369d11e8bf33ae21ca3c4d7e52308
Jason Bayton <jason@bayton.org>
2021-04-12 00:29:41 +0100

    update layout

--- _ct/android.php
+++ _ct/android.php
@@ -1,70 +1,115 @@
-<section id="archive_posts">
-  <h2 id="archive_title">
-    Android Enterprise
-  </h2>
-  <div id="archive_posts_container">
-
-    <div id="android_post_container">
-      <div class="android-topic">
-        <div class="android-topic-title">
-          Getting started
-        </div>
-        <?php
-        foreach ($posts as $query){
-          if (in_array('Getting started', $query['topic']) && $query['published'] == "true") {
-            echo '<li><a href="' . $query['url'] . '">' . $query['title'] . '</a></li>';
-          };
-        } ?>
-      </div>
-
-      <div class="android-topic">
-        <div class="android-topic-title">
-          Diving deeper
-        </div>
-        <?php
-        foreach ($posts as $query){
-          if (in_array('Diving deeper', $query['topic']) && $query['published'] == "true") {
-            echo '<li><a href="' . $query['url'] . '">' . $query['title'] . '</a></li>';
-          };
-        } ?>
-      </div>
-
-      <div class="android-topic">
-        <div class="android-topic-title">
-          Zero-touch
-        </div>
-        <?php
-        foreach ($posts as $query){
-          if (in_array('Zero-touch', $query['topic']) && $query['published'] == "true") {
-            echo '<li><a href="' . $query['url'] . '">' . $query['title'] . '</a></li>';
-          };
-        } ?>
-      </div>
-
-      <div class="android-topic">
-        <div class="android-topic-title">
-          App management
-        </div>
-        <?php
-        foreach ($posts as $query){
-          if (in_array('App management', $query['topic']) && $query['published'] == "true") {
-            echo '<li><a href="' . $query['url'] . '">' . $query['title'] . '</a></li>';
-          };
-        } ?>
-      </div>
-
-      <div class="android-topic">
-        <div class="android-topic-title">
-          Vendor specific
-        </div>
-        <?php
-        foreach ($posts as $query){
-          if (in_array('Vendor specific', $query['topic']) && $query['published'] == "true") {
-            echo '<li><a href="' . $query['url'] . '">' . $query['title'] . '</a></li>';
-          };
-        } ?>
-      </div>
+<section id="content_container">
+  <aside>
+    <?php include "$themedir/_tc/aside.php"; ?>
+  </aside>
+  <article>
+    <?php include "$themedir/_tc/breadcrumb.php"; ?>
+    <div id="article_meta">
+      <i class="fas fa-calendar"></i> <?php echo $published;?> | <i class="fas fa-edit"></i> <?php echo $updated;?> | <i class="fas fa-clock"></i> <?php if ($readtime == 1) {
+            echo $readtime . ' minute';
+            } else {
+            echo $readtime . ' minutes';
+            } ?>
     </div>
+    <h2 id="article_title">
+      <?php echo $post['title'];?>
+    </h2>
+    <?php if (!empty($post['subtitle'])) {
+    echo '<div id="article_subtitle">' . $post['subtitle'] . '</div>';
+    } ?>
+    <?php if (!empty($post['featuredImage'])) {
+    echo '<div id="featured_image"><img src="' . $post['featuredImage'] . '"></div>';
+    } ?>
+
+    <div id="literal_content" class="js-toc-content">
+
+      <div id="android_post_container">
+        <div class="android-topic">
+          <div class="android-topic-title">
+            Getting started
+          </div>
+          <?php
+          foreach ($posts as $query){
+            if (in_array('Getting started', $query['topic']) && $query['published'] == "true") {
+              echo '<li><a href="' . $query['url'] . '">' . $query['title'] . '</a></li>';
+            };
+          } ?>
+        </div>
+
+        <div class="android-topic">
+          <div class="android-topic-title">
+            Diving deeper
+          </div>
+          <?php
+          foreach ($posts as $query){
+            if (in_array('Diving deeper', $query['topic']) && $query['published'] == "true") {
+              echo '<li><a href="' . $query['url'] . '">' . $query['title'] . '</a></li>';
+            };
+          } ?>
+        </div>
+
+        <div class="android-topic">
+          <div class="android-topic-title">
+            Zero-touch
+          </div>
+          <?php
+          foreach ($posts as $query){
+            if (in_array('Zero-touch', $query['topic']) && $query['published'] == "true") {
+              echo '<li><a href="' . $query['url'] . '">' . $query['title'] . '</a></li>';
+            };
+          } ?>
+        </div>
+
+        <div class="android-topic">
+          <div class="android-topic-title">
+            App management
+          </div>
+          <?php
+          foreach ($posts as $query){
+            if (in_array('App management', $query['topic']) && $query['published'] == "true") {
+              echo '<li><a href="' . $query['url'] . '">' . $query['title'] . '</a></li>';
+            };
+          } ?>
+        </div>
+
+        <div class="android-topic">
+          <div class="android-topic-title">
+            Vendor specific
+          </div>
+          <?php
+          foreach ($posts as $query){
+            if (in_array('Vendor specific', $query['topic']) && $query['published'] == "true") {
+              echo '<li><a href="' . $query['url'] . '">' . $query['title'] . '</a></li>';
+            };
+          } ?>
+        </div>
+
+        <div class="android-topic">
+          <div class="android-topic-title">
+            Resources
+          </div>
+          <?php
+          foreach ($posts as $query){
+            if (in_array('Resources', $query['topic']) && $query['published'] == "true") {
+              echo '<li><a href="' . $query['url'] . '">' . $query['title'] . '</a></li>';
+            };
+          } ?>
+        </div>
+
+        <div class="android-topic">
+          <div class="android-topic-title">
+            Articles
+          </div>
+          <?php
+          foreach ($posts as $query){
+            if (in_array('enterprise', $query['categories']) && $query['published'] == "true") {
+              echo '<li><a href="' . $query['url'] . '">' . $query['title'] . '</a></li>';
+            };
+          } ?>
+        </div>
+      </div>
 
     </div>
-  </section>
+    <?php include "$themedir/_tc/share.php"; ?>
+  </article>
+</section>
