aad929d23481d822a254d8b9479b0de7dbd86346
Jason Bayton <jason@bayton.org>
2021-04-11 23:39:59 +0100

    debug

--- _ct/android.php
+++ _ct/android.php
@@ -7,16 +7,16 @@
     <?php
       foreach ($posts as $query){
       foreach ($query['topic'] as $topic) {
-      $unique = array_unique($topic); ?>
-      <div class="related-post-column">
-      <?php echo '<div class="tag-title">'.$unique.'</div>'; ?>
-      <ul class="related-posts-list">
-      <?php foreach ($posts as $query){
-       if (in_array($topic, $query['topic']) && $query['published'] == "true") {
-            echo '<li><a href="' . $query['url'] . '">' . $query['title'] . '</a></li>';
-          }}; ?>
-        </ul>
-      </div>
+        $unique = array_unique($topic);?>
+        <div class="related-post-column">
+          <?php echo '<div class="tag-title">'.$unique.'</div>'; ?>
+          <ul class="related-posts-list">
+          <?php foreach ($posts as $query){
+           if (in_array($topic, $query['topic']) && $query['published'] == "true") {
+                echo '<li><a href="' . $query['url'] . '">' . $query['title'] . '</a></li>';
+              }}; ?>
+            </ul>
+          </div>
     <?php } } ?>
 
     </div>
