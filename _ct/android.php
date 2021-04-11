c66dee5506a293b9b2fd1cee3c916fbd719589bf
Jason Bayton <jason@bayton.org>
2021-04-12 00:07:22 +0100

    the devil I know..

--- _ct/android.php
+++ _ct/android.php
@@ -4,20 +4,52 @@
   </h2>
   <div id="archive_posts_container">
 
-    <?php
-      foreach ($posts as $query){
-      foreach ($query['topic'] as $topic) {
-        $unique = array_unique($topic);?>
-        <div class="related-post-column">
-          <?php echo '<div class="tag-title">'.$unique.'</div>'; ?>
-          <ul class="related-posts-list">
-          <?php foreach ($posts as $query){
-           if (in_array($topic, $query['topic']) && $query['published'] == "true") {
-                echo '<li><a href="' . $query['url'] . '">' . $query['title'] . '</a></li>';
-              }}; ?>
-            </ul>
-          </div>
-    <?php } } ?>
+    <div id="android_post_container">
+      <div class="android-topic">
+        <?php
+        foreach ($posts as $query){
+          if (in_array('Getting started', $query['topic']) && $query['published'] == "true") {
+            echo '<li><a href="' . $query['url'] . '">' . $query['title'] . '</a></li>';
+          };
+        } ?>
+      </div>
+
+      <div class="android-topic">
+        <?php
+        foreach ($posts as $query){
+          if (in_array('Diving deeper', $query['topic']) && $query['published'] == "true") {
+            echo '<li><a href="' . $query['url'] . '">' . $query['title'] . '</a></li>';
+          };
+        } ?>
+      </div>
+
+      <div class="android-topic">
+        <?php
+        foreach ($posts as $query){
+          if (in_array('Zero-touch', $query['topic']) && $query['published'] == "true") {
+            echo '<li><a href="' . $query['url'] . '">' . $query['title'] . '</a></li>';
+          };
+        } ?>
+      </div>
+
+      <div class="android-topic">
+        <?php
+        foreach ($posts as $query){
+          if (in_array('App management', $query['topic']) && $query['published'] == "true") {
+            echo '<li><a href="' . $query['url'] . '">' . $query['title'] . '</a></li>';
+          };
+        } ?>
+      </div>
+
+      <div class="android-topic">
+        <?php
+        foreach ($posts as $query){
+          if (in_array('Vendor specific', $query['topic']) && $query['published'] == "true") {
+            echo '<li><a href="' . $query['url'] . '">' . $query['title'] . '</a></li>';
+          };
+        } ?>
+      </div>
+    </div>
 
     </div>
   </section>
