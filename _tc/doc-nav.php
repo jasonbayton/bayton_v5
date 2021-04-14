dbaafe4b4cd31b7bb96cade7fdd34954728f8422
Jason Bayton <jason@bayton.org>
2021-04-15 00:53:53 +0100

    testing

--- _tc/doc-nav.php
+++ _tc/doc-nav.php
@@ -2,7 +2,7 @@
   <div class="aside-title">
     <h2>Documents</h2>
   </div>
-  <?php foreach ($posts as $query){};?>
+  <?php foreach ($posts as $query){?>
   <?php echo '<!--'.$query['topic'].'-->';
   if ($post['topic'] == $query['topic']) { ?>
   <details open class="android-topic">
@@ -14,14 +14,12 @@
     </summary>
     <ul>
     <?php
-    foreach ($posts as $query){
       if ($query['parent'] == "Android" && $query['topic'] == "Getting started" && $query['published'] == "true") {
         echo '<li><a href="' . $query['url'] . '">' . $query['title'] . '</a></li>';
-      };
-    } ?>
+      }; ?>
     </ul>
   </details>
-
+<?php } ?>
   <?php if ($post['topic'] == $query['topic']) { ?>
   <details open class="android-topic">
   <?php } else { ?>
