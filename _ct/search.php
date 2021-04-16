dea96cdadddcf549ff81255ff7298a7d08c8e749
Jason Bayton <jason@bayton.org>
2021-04-16 18:07:48 +0100

    avoiding case issues

--- _ct/search.php
+++ _ct/search.php
@@ -1,5 +1,5 @@
 <?php
-$search = $_GET['keyword'];
+$search = strtolower($_GET['keyword']);
 echo $search;
 ?>
 
@@ -10,7 +10,7 @@ echo $search;
     </h2>
     <?php
     foreach ($posts as $query){
-      if ($query['parent'] == $search || $query['topic'] == $search ) {
+      if (strtolower($query['parent']) == $search || strtolower($query['topic']) == $search ) {
         echo '<li><!--' . $query['order'] . '--><a href="' . $query['url'] . '">' . $query['title'] . '</a></li>';
     } else {
       echo 'No matches have been found for your search';
