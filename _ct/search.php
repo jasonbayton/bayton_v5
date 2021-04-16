082833e517da5a4099ee3654d9da87ed67cbe3eb
Jason Bayton <jason@bayton.org>
2021-04-16 18:41:20 +0100

    uno reverse

--- _ct/search.php
+++ _ct/search.php
@@ -22,7 +22,7 @@ $search = strtolower($_GET['keyword']);
         echo '<li><!--' . $query['order'] . '--><a href="' . $query['url'] . '">' . $query['title'] . '</a></li>';
         }
       }
-      if (!strpos($query['title'], $search)){
+      if (strpos($query['title'], $search)){
         echo '<li><!--' . $query['order'] . '--><a href="' . $query['url'] . '">' . $query['title'] . '</a></li>';
       }
     };
