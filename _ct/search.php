d641c36884391edc90ab81dce3d7c00d57c25fca
Jason Bayton <jason@bayton.org>
2021-04-16 22:36:04 +0100

    tidy up

--- _ct/search.php
+++ _ct/search.php
@@ -1,7 +1,6 @@
 <?php
 $search = strtolower($_GET['keyword']);
 ?>
-
 <section id="page_container">
 	<article>
 		<h2 id="article_title">
@@ -11,34 +10,29 @@ $search = strtolower($_GET['keyword']);
       <?php echo 'Results for: "' . $search . '"' ?>
     </h3>
     <ul>
-		<?php
-		$exactMatch = [];
-		foreach ($posts as $post) {
-			$exactMatch[] = searchArray($post, explode(' ', $_GET['keyword']), true);
-		}
-
-		foreach (array_filter($exactMatch) as $result) {
-			echo '<li><!--' . $result['order'] . '--><a href="' . $result['url'] . '">' . $result['title'] . '</a></li>';
-		}
-
-		?>
+  		<?php
+  		$exactMatch = [];
+  		foreach ($posts as $post) {
+  			$exactMatch[] = searchArray($post, explode(' ', $_GET['keyword']), true);
+  		}
+  		foreach (array_filter($exactMatch) as $result) {
+  			echo '<li><!--' . $result['order'] . '--><a href="' . $result['url'] . '">' . $result['title'] . '</a></li>';
+  		}
+      ?>
     </ul>
 		<h3 class="search-heading">
 			Similar Results based on: "<?php echo $search; ?>"
 		</h3>
     <ul>
-		<?php
-
-		$closeMatch = [];
-		foreach ($posts as $post) {
-			$closeMatch[] = searchArray($post, explode(' ', $_GET['keyword']), false);
-		}
-
-		foreach (array_filter($closeMatch) as $result) {
-			echo '<li><!--' . $result['order'] . '--><a href="' . $result['url'] . '">' . $result['title'] . '</a></li>';
-		}
-
-    ?>
+  		<?php
+  		$closeMatch = [];
+  		foreach ($posts as $post) {
+  			$closeMatch[] = searchArray($post, explode(' ', $_GET['keyword']), false);
+  		}
+  		foreach (array_filter($closeMatch) as $result) {
+  			echo '<li><!--' . $result['order'] . '--><a href="' . $result['url'] . '">' . $result['title'] . '</a></li>';
+  		}
+      ?>
     </ul>
     <?php
 		function searchArray(array $haystack, array $needles, bool $direct = true) {
@@ -47,8 +41,6 @@ $search = strtolower($_GET['keyword']);
 			foreach ($needles as $needle) {
 				$matches[$needle] = 0;
 			}
-
-
 			foreach ($needles as $needle) {
 				foreach ($haystack as $row) {
 					if (!is_array($row) && stripos($row, $needle)) {
@@ -62,19 +54,15 @@ $search = strtolower($_GET['keyword']);
 					}
 				}
 			}
-
 			$filteredArray = array_filter($matches);
 			$matchCount = count($filteredArray);
-
 			if ($direct === false && $matchCount !== 0 && $matchCount !== $needleCount) {
 				return $haystack;
 			}
-
 			if ($direct === true && $matchCount !== 0 && $needleCount === $matchCount) {
 				return $haystack;
 			}
 		}
-
 		?>
 		</div>
 	</article>
