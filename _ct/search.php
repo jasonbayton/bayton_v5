cd3684322d83acdd20efd20c3b9f4cf1788ce6de
Jason Bayton <jason@bayton.org>
2021-04-16 18:18:51 +0100

    missing closing brackets

--- _ct/search.php
+++ _ct/search.php
@@ -10,17 +10,17 @@ echo $search;
     </h2>
     <?php
     foreach ($posts as $query){
-      if (strtolower($query['parent']) == $search || strtolower($query['topic']) == $search ) {
+      if (strtolower($query['parent']) == $search || strtolower($query['topic']) == $search) {
         echo '<li><!--' . $query['order'] . '--><a href="' . $query['url'] . '">' . $query['title'] . '</a></li>';
       }
       foreach ($query['categories'] as $category){
-        if (strtolower($category) == $search {
+        if (strtolower($category) == $search) {
           echo '<li><!--' . $query['order'] . '--><a href="' . $query['url'] . '">' . $query['title'] . '</a></li>';
         }
       }
       foreach ($query['tags'] as $tag){
-        if (strtolower($tag) == $search {
-          echo '<li><!--' . $query['order'] . '--><a href="' . $query['url'] . '">' . $query['title'] . '</a></li>';
+        if (strtolower($tag) == $search) {
+        echo '<li><!--' . $query['order'] . '--><a href="' . $query['url'] . '">' . $query['title'] . '</a></li>';
         }
       }
     }
