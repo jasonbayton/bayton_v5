8e422981b9458ea2bb0f2b80427b3dfa241c008d
Jason Bayton <jason@bayton.org>
2021-04-17 22:55:06 +0100

    stripping back

--- _tc/featured_doc.php
+++ _tc/featured_doc.php
@@ -1,18 +1,13 @@
-  <section id="featured">
-    <svg class="angled-background-top purple-background" preserveAspectRatio="none" viewBox="0, 0, 100, 10">
-      <polygon class="left-triangle orange-cross" points="0,0 0,100 100,10"></polygon>
-      <polygon class="right-triangle" points="100,100 100,0 0,10"></polygon>
-    </svg>
     <div class="primary-background">
       <div id="featured_container" class="criss-cross orange-cross criss-cross-mid">
-        <span id="featured_text">Featured article</span>
+        <span id="featured_text">Featured doc</span>
         <?php
         $i=0;
         foreach ($posts as $query) if ($query['featured_doc'] == 'true' && $query['published'] == "true"){
             $i++;
             if($i==2) break; ?>
             <h2 id="featured_article">
-              <?php echo $query['title']; ?>
+              <?php echo '<a href="' . $query['url'] . '">' . $query['title'] . '</a>'; ?>
             </h2>
             <button id="embedded_readmore" class="button-blue button-rounded">
               <a href="<?php echo $query['url'] ?>">Read more</a></i>
@@ -20,9 +15,3 @@
             <?php } ?>
           </div>
       </div>
-    <svg class="angled-background-bottom" preserveAspectRatio="none" viewBox="0, 0, 100, 10">
-    	<polygon class="left-triangle orange-cross" points="100,0 0,0, 100,10"></polygon>
-      <polygon class="shadow-triangle" points="0,10 100,0, 100,0.4"></polygon>
-    	<polygon class="right-triangle" points="100,0 0,0, 0,10"></polygon>
-    </svg>
-  </section>
