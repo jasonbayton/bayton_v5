47ba4370438e5dd42bc409a503dd929e58f9a044
Jason Bayton <jason@bayton.org>
2021-04-19 13:24:07 +0100

    add id to search

--- _tc/nav.php
+++ _tc/nav.php
@@ -1,6 +1,6 @@
 <div id="search">
   <span class="fa fa-search" id="search_icon"></span>
-  <input type="text" label="search" placeholder="What would you like to learn today?" method="GET">
+  <input type="text" label="search" id="searchinput" placeholder="What would you like to learn today?" method="GET">
 </div>
 <div id="nav_links">
   <a href="/about">About</a> <a href="/contact">Contact</a>
