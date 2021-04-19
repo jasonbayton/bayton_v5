1ebe8dc775cf682acb290b2361f93ad09629d027
Jason Bayton <jason@bayton.org>
2021-04-19 13:20:06 +0100

    search method

--- _tc/nav.php
+++ _tc/nav.php
@@ -1,6 +1,6 @@
 <div id="search">
   <span class="fa fa-search" id="search_icon"></span>
-  <input type="text" label="search" placeholder="What would you like to learn today?">
+  <input type="text" label="search" placeholder="What would you like to learn today?" method="GET">
 </div>
 <div id="nav_links">
   <a href="/about">About</a> <a href="/contact">Contact</a>
