9e4d8d5dd941fb3dca39061000aadeeec92272a4
Jason Bayton <jason@bayton.org>
2021-04-19 13:35:54 +0100

    rejig the atts

--- _tc/nav.php
+++ _tc/nav.php
@@ -1,6 +1,6 @@
-<form id="searchform" method="GET">
+<form id="searchform" method="GET" action="/search">
   <span class="fa fa-search" id="search_icon"></span>
-  <input type="text" label="search" id="searchinput" placeholder="What would you like to learn today?">
+  <input type="text" name="keyword" id="searchinput" placeholder="What would you like to learn today?">
 </form>
 <div id="nav_links">
   <a href="/about">About</a> <a href="/contact">Contact</a>
