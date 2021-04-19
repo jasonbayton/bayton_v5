94f8b3c3ff531772dd2f7d683d611ec141c49bb3
Jason Bayton <jason@bayton.org>
2021-04-19 12:41:55 +0100

    move search

--- _tc/nav.php
+++ _tc/nav.php
@@ -1,10 +1,10 @@
+<div id="search">
+  <span class="fa fa-search" id="search_icon"></span>
+  <input type="text" label="search" placeholder="What would you like to learn today?">
+</div>
 <div id="nav_links">
   <a href="/about">About</a> <a href="/contact">Contact</a>
 </div>
 <div id="darktoggle">
-  &#9680;
-</div>
-<div id="search">
-  <span class="fa fa-search" id="search_icon"></span>
-  <input type="text" label="search" placeholder="What would you like to learn today?">
+  <i class="fas fa-clouds-moon"></i>
 </div>
