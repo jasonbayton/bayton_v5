3328311fb4ada6f5dc3e46f01c21d54bfda4fe62
Jason Bayton <jason@bayton.org>
2021-04-19 13:44:43 +0100

    revert

--- _tc/nav.php
+++ _tc/nav.php
@@ -1,6 +1,6 @@
 <div id="searchform">
   <span class="fa fa-search" id="search_icon"></span>
-  <input type="text" name="keyword" id="searchinput" placeholder="What would you like to learn today?">
+  <input type="text" name="search" id="searchinput" placeholder="What would you like to learn today?">
 </div>
 <div id="nav_links">
   <a href="/about">About</a> <a href="/contact">Contact</a>
