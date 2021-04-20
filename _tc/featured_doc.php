db181be9b2bbbcc2b75b70bfd6106a94acbada2e
Jason Bayton <jason@bayton.org>
2021-04-20 22:27:06 +0100

    tidy up featured

--- _tc/featured_doc.php
+++ _tc/featured_doc.php
@@ -1,17 +1,14 @@
-    <div class="primary-background">
       <div id="featured_container" class="criss-cross orange-cross criss-cross-mid">
+        <hr>
         <span id="featured_text">Featured doc</span>
         <?php
         $i=0;
         foreach ($posts as $query) if ($query['featured_doc'] == 'true' && $query['published'] == "true"){
-            $i++;
-            if($i==2) break; ?>
-            <h2 id="featured_article">
-              <?php echo '<a href="' . $query['url'] . '">' . $query['title'] . '</a>'; ?>
-            </h2>
-            <button id="embedded_readmore" class="button-blue button-rounded">
-              <a href="<?php echo $query['url'] ?>">Read more</a></i>
-            </button>
-            <?php } ?>
-          </div>
-      </div>
+          $i++;
+          if($i==2) break; ?>
+          <h2 id="featured_article">
+            <?php echo '<a href="' . $query['url'] . '">' . $query['title'] . '</a>'; ?>
+          </h2>
+          <hr>
+          <?php } ?>
+        </div>
