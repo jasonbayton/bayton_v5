37046e7611319f216d03f5ec3ecdc246b25933de
Jason Bayton <jason@bayton.org>
2022-07-25 22:13:47 +0100

    convert stuff to njk

--- templates_other_cms/__eleventy-ct/home.php
+++
@@ -1,5 +0,0 @@
-<?php include "$themedir/_tc/grandiose_statement.php"; ?>
-<?php include "$themedir/_tc/trusted_logos.php"; ?>
-<?php include "$themedir/_tc/featured_post.php"; ?>
-<?php include "$themedir/_tc/latest_posts.php"; ?>
-<?php include "$themedir/_tc/older_posts.php"; ?>
